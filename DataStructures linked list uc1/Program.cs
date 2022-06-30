e12c6a276f675fcc3e55aab9b5dcb05bace26f49
mani1 <mani1@NANI143>
2022-06-30 15:03:06 +0530

    data structues uc1

---
+++ DataStructures linked list uc1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+namespace DataStructuresDemo
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HelloWorld!");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+        }
+    }
+}
---
+++ DataStructures linked list uc1/class.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace DataStructuresDemo
+{
+    internal class linked_list_add
+    {
+        internal Node head;
+        internal void Add(int data)
+        {
+            Node node = new Node(data)
+            if(this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = head;
+            }
+            Console.WriteLine("{0} inserted into linkedlist()", Node.data);
+        }
+    }
+}
